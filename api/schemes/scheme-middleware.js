79605a95ba83f463a798fb8fa03db900b7720ac4
Eli <elize.san10@gmail.com>
2021-03-17 16:45:26 -0500

    write middleware functions

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -7,8 +7,13 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
-}
+  const { id } = req.params;
+  if (!id) {
+    res.status(404).json({ message: `scheme with scheme_id ${id} not found` });
+  } else {
+    next();
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,8 +24,17 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  const schemeName = req.params.schem_name;
+  if (
+    !schemeName ||
+    schemeName.length === 0 ||
+    typeof schemeName !== "string"
+  ) {
+    res.status(400).json({ message: "invalid schema_name" });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,11 +46,24 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  const instructions = req.params.instructions;
+  const stepNumber = req.params.step_number;
 
-}
+  if (
+    !instructions ||
+    instructions.length === 0 ||
+    typeof instructions !== "string"
+  ) {
+    res.status(400).json({ message: "invalid step" });
+  } else if (typeof stepNumber !== "number" || stepNumber < 1) {
+    res.status(400).json({ message: "invalid step" });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
